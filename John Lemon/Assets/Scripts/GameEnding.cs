f9cd18141335a3d3e3562ee94cee77d91f3338de
kanthyr <49036253+kanthyr@users.noreply.github.com>
2020-09-18 16:24:24 -0500

    John Lemon Finished

---
+++ John Lemon/Assets/Scripts/GameEnding.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    [SerializeField] private float fadeDuration = 1f;
+    [SerializeField] private float displayFadeDuration = 1f;
+    [SerializeField] private GameObject player = null;
+    [SerializeField] private CanvasGroup exitBackGroundImageCanvasGroup = null;
+    [SerializeField] private CanvasGroup caughtBackGroundImageCanvasGroup = null;
+    [SerializeField] private AudioSource exitAudio = null;
+    [SerializeField] private AudioSource caughtAudio = null;
+
+    private bool hasAudioPlayed;
+
+    private float timer;
+
+    private bool isPlayerAtExit;
+    private bool isPlayerCaught;
+
+    private void Update()
+    {
+        if(isPlayerAtExit)
+        {
+            EndLevel(exitBackGroundImageCanvasGroup, false, exitAudio);
+        }
+        else if (isPlayerCaught)
+        {
+            EndLevel(caughtBackGroundImageCanvasGroup, true, caughtAudio);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            isPlayerAtExit = true;
+        }
+    }
+
+    /// <summary>
+    /// Al invocarse muestra la imagen de fin de la partida y cierra el juego
+    /// </summary>
+    /// <param name="imageCanvasGroup">Imagen de fin de partida correspondiente</param>
+    /// <param name="doRestart"></param>
+    /// <param name="audioSource"></param>
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart, AudioSource audioSource)
+    {
+        if (!hasAudioPlayed)
+        {
+            audioSource.Play();
+            hasAudioPlayed = true;
+        }
+
+        timer += Time.deltaTime;
+        imageCanvasGroup.alpha = Mathf.Clamp(timer / fadeDuration, 0, 1);
+
+        if (timer > fadeDuration + displayFadeDuration)
+        {
+            if(doRestart)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void CatchPlayer()
+    {
+        isPlayerCaught = true;
+    }
+}
---
+++ John Lemon/Assets/Scripts/Observer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CapsuleCollider))]
+public class Observer : MonoBehaviour
+{
+    private Transform playerTransform = null;
+    private GameEnding gameEnding = null;
+
+    private bool isPlayerInRange;
+
+    private void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+        gameEnding = GameObject.FindObjectOfType<GameEnding>();
+    }
+
+    private void Update()
+    {
+        if (isPlayerInRange)
+        {
+            Vector3 direction = playerTransform.position - this.transform.position + Vector3.up;
+            Ray ray = new Ray(this.transform.position, direction);
+
+            Debug.DrawRay(this.transform.position, direction, Color.magenta, Time.deltaTime, true);
+
+            RaycastHit raycastHit;
+            if (Physics.Raycast(ray, out raycastHit))
+            {
+                if(raycastHit.collider.transform == playerTransform)
+                {
+                    gameEnding.CatchPlayer();
+                }
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == playerTransform)
+        {
+            isPlayerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == playerTransform)
+        {
+            isPlayerInRange = false;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(this.transform.position, 0.1f);
+    }
+}
