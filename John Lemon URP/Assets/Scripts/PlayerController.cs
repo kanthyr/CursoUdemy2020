366a39adb4ba90fc7d07dff04cee0228d0f71f96
kanthyr <49036253+kanthyr@users.noreply.github.com>
2020-09-18 20:21:10 -0500

    John Lemon URP Añadido

---
+++ John Lemon URP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 20f;
+
+    private Vector3 movement;
+    private Quaternion rotation = Quaternion.identity;
+
+    private Animator _animator;
+    private Rigidbody _rigidbody;
+    private AudioSource _audioSource;
+
+    void Start()
+    {
+        _animator = this.GetComponent<Animator>();
+        _rigidbody = this.GetComponent<Rigidbody>();
+        _audioSource = this.GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        movement.Set(horizontal, 0, vertical);
+        movement.Normalize();
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+     
+        _animator.SetBool("isWalking", isWalking);
+
+        if (isWalking)
+        {
+            if (!_audioSource.isPlaying)
+            {
+                _audioSource.Play();
+            }
+        }
+        else
+        {
+            _audioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.fixedDeltaTime, 0f);
+
+        rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    private void OnAnimatorMove()
+    {
+        _rigidbody.MovePosition(_rigidbody.position + movement * _animator.deltaPosition.magnitude);
+        _rigidbody.MoveRotation(rotation);
+    }
+}
---
+++ John Lemon URP/Assets/Scripts/WaypointPatrol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class WaypointPatrol : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints = new Transform[0];
+
+    private NavMeshAgent _navMeshAgent;
+
+    private int currentWaypointIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        _navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_navMeshAgent.remainingDistance < _navMeshAgent.stoppingDistance)
+        {
+            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+            _navMeshAgent.SetDestination(waypoints[currentWaypointIndex].position);
+        }
+    }
+}
